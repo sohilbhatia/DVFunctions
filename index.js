5e8b62f3355e6939da6841be20a975d686d652ac
Sohil Bhatia <65570319+sohilbhatia@users.noreply.github.com>
2021-03-31 00:27:58 -0700

    Update index.js
    final version to send parent notifications

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-const myToken = ();
-exports.newNodeUpdated = functions.database.ref("parties/users")
-    .onWrite((event) => {
+const finder = "codes/{userCode}/messages/message";
+exports.newNodeUpdated = functions.database.ref(finder)
+    .onWrite((change, context) => {
+      const tMessage = change.after.val();
+      console.log(tMessage);
       const payload = {
         notification: {
-          body: "Check test",
-          title: "Postman just worked",
+          body: "Your " + tMessage,
+          title: "Teenage Driver Update",
           content_available: "true",
         },
         data: {
@@ -17,83 +19,19 @@ exports.newNodeUpdated = functions.database.ref("parties/users")
           content_available: "true",
         },
       };
-      /*
-      const pay = {
-        notification: {
-          title: "Message from your Teenage Driver",
-          body: "check it out",
-          badge: "1",
-          sound: "default",
-          content_available: "true",
-        },
-      };
-      */
       const op = {
         priority: "high",
       };
-      // const so = "parties/users/status/myToken";
-      const l = Object.keys(myToken);
-      if (l != null) {
-        console.log("Token is good");
-      }
-      return admin.messaging().sendToDevice(myToken, payload, op)
-          .then(function(response) {
-            console.log("Success!");
-          })
-          .catch(function(error) {
-            console.log("Error: " + error);
-          });
-      // console.log(updated);
-      /*
-      return admin.database().ref(so).once("value").then((snap) => {
-        if (snap.val() != null) {
-          console.log("WORKING");
-        }
-      });
-      */
-      /*
-      return admin.database().ref("parties" + so).once("value").then((snap) => {
-        admin.messaging().sendToDevice(myToken, payload)
-            .then(function(response) {
-              console.log("Successfully sent message:", response);
-            })
-            .catch(function(error) {
-              console.log("Error sending message:", error);
-            });
-      });
-      */
-    });
-/*
-exports.newNodeDetected = functions.database.ref(
-    "codes/{userCode}/messages/message")
-    .onCreate((snapshot, context) => {
-      const name = snapshot.val();
-      const userCode = context.params.userCode;
-      console.log(userCode + " received a message: " + name);
-      const box = snapshot.val();
-      const ne = context.params.userCode;
-      console.log(ne + " updated a message: " + box);
-      return admin.database().ref("codes/" + ne).once("value").then((snap) => {
+      const location = ("codes/" + context.params.userCode);
+      return admin.database().ref(location).once("value").then((snap) => {
         const token = snap.child("parentToken").val();
-        console.log("token: ", token);
-        console.log("Constructing the notification message.");
-        const payload = {
-          data: {
-            title: "Message from your Teenage Driver",
-            body: "IT WORKED!",
-          },
-        };
-        const options = {
-          priority: "high",
-          timeToLive: 1,
-        };
-        admin.messaging().sendToDevice(token, payload, options)
+        admin.messaging().sendToDevice(token, payload, op)
             .then(function(response) {
-              console.log("Successfully sent message:", response);
+              console.log("Success!");
             })
             .catch(function(error) {
-              console.log("Error sending message:", error);
+              console.log("Error: " + error);
             });
       });
     });
-*/
+
